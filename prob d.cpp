9c1da6aad7ad95bd0bd8ea4b47d5a2ee8a1c559f
tasmima <tasmima29@gmail.com>
2025-05-04 23:59:29 +0600

    files deleted

--- prob d.cpp
+++
@@ -1,9 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main()
-{
- long long a,b,c,d,x;
- cin>>a>>b>>c>>d;
- x=(a*b)-(c*d);
- cout<<"Difference = "<<x<<endl;
-}
--- prob f.cpp
+++
@@ -1,9 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main()
-{
- long long a,b,c;
- cin>>a>>b;
- c = a%10 + b%10;
- cout<<c<<endl;
-}
