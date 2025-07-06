d0018d78218940e2658a92632e69c24e35509a8f
tasmima <tasmima29@gmail.com>
2025-07-07 00:17:37 +0600

    new file uplode

---
+++ sheet_4(string)/prob A.cpp
@@ -0,0 +1,16 @@
+//course name: Competitive Programming
+//course code:2310
+//course teacher name: Mirza Raquib
+//date:23/6/25
+//Problem name:A - Create A New String
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    string s,t;
+    cin>>s>>t;
+    cout<<s.size()<<" " <<t.size()<<endl;
+    cout<<s<<" " <<t<<endl;
+
+}
